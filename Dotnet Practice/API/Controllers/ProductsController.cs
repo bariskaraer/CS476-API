8fe049cb17f35e0771c6bfa39e9e8762a6433334
Barış Karaer <baris.karaer@ozu.edu.tr>
2021-03-30 17:23:28 +0300

    Product Controller

---
+++ Dotnet Practice/API/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+using System.Threading.Tasks;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class ProductsController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public ProductsController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpPost("add")]
+        public async Task<ActionResult<Product>> Add(ProductDTO productDTO){
+            if(await ProductExists(productDTO.productName)){
+                return BadRequest("Product Name is taken");
+            }
+
+
+            var product = new Product{
+                productName = productDTO.productName.ToLower(),
+                Price = productDTO.Price,
+                Description = productDTO.Description,
+                Comments = productDTO.Comments,
+                Category = productDTO.Category
+            };
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+
+        private async Task<bool> ProductExists(string productName){
+            return await _context.Products.AnyAsync(x => x.productName == productName.ToLower());
+        }
+    }
+}
---
+++ Dotnet Practice/API/DTOs/ProductDTO.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        public string productName  { get; set; } 
+        [Required]
+        public int Price { get; set; }
+        public string Description { get; set; }
+        public string Comments { get; set; }
+        [Required]
+        public string Category {get; set;}
+    }
+}
